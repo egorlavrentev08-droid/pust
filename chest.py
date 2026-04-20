7d5c9d93f65c7227d2d2aebc6690ba5ca130a466
egorlavrentev08-droid <egorlavrentev08@gmail.com>
2026-04-20 07:01:54 +0300

    Update chest.py

--- chest.py
+++ chest.py
@@ -1,6 +1,8 @@
 # chest.py - Сундуки и фабрики
 # Версия: 2.0.0
 
+from telegram import Update
+from telegram.ext import ContextTypes
 from config import logger, FACTORIES
 from core import send_to_private, is_admin
 from database import Session, User
